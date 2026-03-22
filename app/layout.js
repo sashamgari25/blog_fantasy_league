dcf81f5501e8a9a8c9965c21e3dca300e1057e60
sashamgari25 <sainischalreddyashamgari@gmail.com>
2026-03-22 21:30:04 +1100

    removed image url field

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
@@ -11,6 +12,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vzp873npjd");
+          `}
+        </Script>
         <Analytics />
       </body>
     </html>
